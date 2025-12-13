bb165520058fd886f80aad5db1c92db0784f5586
Chandrut22 <chandrutd22@gmail.com>
2025-12-13 15:13:42 +0530

    update the agent main code

--- agent/app/agent/main.py
+++ agent/app/agent/main.py
@@ -22,6 +22,7 @@ class MainAgent:
         """
         workflow = StateGraph(AgentState)
 
+        # Add Nodes
         workflow.add_node("web_crawler", call_firecrawl)
         workflow.add_node("technical_auditor", call_technical_auditor)
         workflow.add_node("crawling_auditor", call_crawling_auditor)
@@ -31,18 +32,25 @@ class MainAgent:
         workflow.add_node("seo_optimizer", call_seo_optimizer)
         workflow.add_node("report_generator", call_report_generator)
 
-        
+        # Define Edges
+        # Parallel start: Crawler and Technical Auditor run at the same time
         workflow.add_edge(START, "web_crawler")
-        workflow.add_edge(START,"technical_auditor")
-        workflow.add_edge("web_crawler","crawling_auditor")
-        workflow.add_edge("web_crawler","on_page_analyzer")
-        workflow.add_edge("technical_auditor","market_researcher")
-        workflow.add_edge("crawling_auditor","market_researcher")
-        workflow.add_edge("on_page_analyzer","market_researcher")
-        workflow.add_edge("market_researcher","seo_strategist")
-        workflow.add_edge("seo_strategist","seo_optimizer")
-        workflow.add_edge("seo_optimizer","report_generator")
-        workflow.add_edge("report_generator",END)
+        workflow.add_edge(START, "technical_auditor")
+        
+        # Crawler feeds into Crawling Auditor and On-Page Analyzer
+        workflow.add_edge("web_crawler", "crawling_auditor")
+        workflow.add_edge("web_crawler", "on_page_analyzer")
+        
+        # Feed all data into Market Researcher
+        workflow.add_edge("technical_auditor", "market_researcher")
+        workflow.add_edge("crawling_auditor", "market_researcher")
+        workflow.add_edge("on_page_analyzer", "market_researcher")
+        
+        # Strategy -> Optimization -> Reporting
+        workflow.add_edge("market_researcher", "seo_strategist")
+        workflow.add_edge("seo_strategist", "seo_optimizer")
+        workflow.add_edge("seo_optimizer", "report_generator")
+        workflow.add_edge("report_generator", END)
 
         return workflow.compile()
 
